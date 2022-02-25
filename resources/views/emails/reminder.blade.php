f0c91cbd324884dc08d1d6aba2e350df5d3e2381
nalisa vianti <nalisa@gmail.com>
2022-02-25 22:08:11 +0700

    update email

--- resources/views/emails/reminder.blade.php
+++ resources/views/emails/reminder.blade.php
@@ -2,9 +2,7 @@
 <h2>Halo, {{ $nama }} !</h2>
 <p> {{ $intro }}</p>
 <p> Segera penuhi persyaratan dibawah ini : </p>
-<ol>
-    <li> {!! $content !!}</li>
-</ol>
+<ol>{!! $content !!}</ol>
 <br>
 <p>{{ $closing }}</p>
 <p> Cek informasi lebih lanjut di link berikut {{ $website }}</p>
